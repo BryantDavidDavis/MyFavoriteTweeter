0a18723fc74a6bdc2fb4f5090182d98d2d85c012
BryantDavidDavis <davis.bryant@gmail.com>
2015-08-05 19:50:41 -0700

    adds gitignore and api prefix to route

--- lib/MyFavoriteTweeter.pm
+++ lib/MyFavoriteTweeter.pm
@@ -3,8 +3,10 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index';
+prefix '/api/1.0' => sub {
+    get '/' => sub {
+        return 'hello my fellow friends';
+    };
 };
 
 true;
