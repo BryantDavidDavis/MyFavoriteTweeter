ee9078d3eac3fe1226ece9a9b57c46a89e7698af
BryantDavidDavis <davis.bryant@gmail.com>
2015-08-10 18:57:38 -0700

    cleans up twitterhandler code

--- lib/TwitterHandler.pm
+++ lib/TwitterHandler.pm
@@ -9,7 +9,7 @@ use Array::Utils;
 
 #authentication_keys belong in $ENV variables
 my $nt = Net::Twitter->new(
-    traits   => [qw/API::RESTv1_1/],
+    traits   => [qw/AutoCursor API::RESTv1_1 RetryOnError OAuth/],
     consumer_key        => $MySecretKeys::consumer_key,
     consumer_secret     => $MySecretKeys::consumer_secret,
     access_token        => $MySecretKeys::access_token,
@@ -17,26 +17,20 @@ my $nt = Net::Twitter->new(
 );
 
 sub get_tweets {
+
     my ($user_name) = @_;
+
     return $nt->user_timeline({screen_name => $user_name});
 }
 
 sub get_intersection {
+
     my ($user_one, $user_two) = @_;
-    my @ids_one;
-    my @ids_two;
 
-    for ( my $cursor = -1, my $r; $cursor; $cursor = $r->{next_cursor} ) {
-        $r = $nt->friends_ids({screen_name => $user_one, cursor => $cursor });
-        push @ids_one, @{$r->{ids} };
-    }
-
-    for ( my $cursor = -1, my $r; $cursor; $cursor = $r->{next_cursor} ) {
-        $r = $nt->friends_ids({screen_name => $user_two, cursor => $cursor });
-        push @ids_two, @{$r->{ids} };
-    }
-
-    my @ids_array = Array::Utils::intersect(@ids_one, @ids_two);
+    my $ids_one = $nt->friends_ids({screen_name => $user_one});
+    my $ids_two = $nt->friends_ids({screen_name => $user_two});
+    my @ids_array = Array::Utils::intersect(@{$ids_one}, @{$ids_two});
+    
     return $nt->lookup_users({user_id => [@ids_array]});
 }
 
